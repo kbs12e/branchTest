05bf9cad418a668631cf48c384c60bd8204d3d8d
bongsub.kim <bongsub.kim@lgepartner.com>
2015-03-10 11:18:39 +0900

    test for sunstone watchmanager branch
    test
    
    Change-Id: Ie699c08ac1f089802cc417851b8ec9f7dfa91222

---
+++ enyo/source/package.js
@@ -0,0 +1,15 @@
+enyo.depends(
+	"kernel",
+	"ext",
+	"ajax",
+	"data",
+	"dom",
+	"touch",
+	"ui"
+	//"enyo.design",
+	// ,
+	// "../../lib/enyo-ilib",
+	// "../../lib/enyo-webos",
+	// "../../lib/garnet",
+	// "../../lib/enyo-cordova/assets/cordova-js-2.7.0"
+);
